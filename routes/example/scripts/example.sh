b118f0b267721a8b908d7557f50a747ef77baf9e
Snagnar <overkill98@web.de>
2019-10-31 08:27:26 +0100

    added example route

---
+++ routes/example/scripts/example.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This is an example script for a query argument parser
+# You can add as many scripts in this directory as you want.
+#
+# But keep in mind that the scripts will be executed proceduraly, so
+# script which take very long to execute might not be the best idea...
+#
+# author: Snagnar
+
+echo "got query arguments: $@"
